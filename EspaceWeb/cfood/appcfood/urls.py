84c9c5742ecf0816f36b06eba09c5128ddebc1f5
CallMeBrubeck <ki.netkambir@gmail.com>
2024-03-19 03:43:22 +0000

    mise a jour des items de la navbar, du fichier acceuil et creation du fichier login pour la connexion

--- EspaceWeb/cfood/appcfood/urls.py
+++ EspaceWeb/cfood/appcfood/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from appcfood import views
 
 urlpatterns = [
-    path('', views.acceuil, name='acceuil')
+    path('', views.acceuil, name='acceuil'),
+    path('menu/', views.menu, name='menu'),
+    path('login', views.login, name='login'),
 ]
--- EspaceWeb/cfood/appcfood/views.py
+++ EspaceWeb/cfood/appcfood/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def acceuil(request):
     return render(request, 'appcfood/acceuil.html')
+def menu(request):
+    return render(request, 'appcfood/menu.html')
+def login(request):
+    return render(request, 'appcfood/login.html')
